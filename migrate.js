fdaec3b9ee665613ff80e7a1bf162e299add3468
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-03-21 13:46:28 +0100

    add migration file to create movies table

---
+++ migrate.js
@@ -0,0 +1,21 @@
+const connect = require('./connect');
+
+const sql = `CREATE TABLE IF NOT EXISTS videos (
+    ID INT unsigned PRIMARY KEY AUTO_INCREMENT,
+    name text NOT NULL,
+    description text NOT NULL,
+    type text NOT NULL,
+    rating INT unsigned DEFAULT 0
+    )`;
+  
+
+  (async function migrateDB() {
+    try {
+      const connection = await connect();
+      await connection.query(sql);
+      console.log('Database has been updated');
+    } catch (err) {
+      console.log('Failed to Update DB');
+      console.log(err);
+    }
+  })();
