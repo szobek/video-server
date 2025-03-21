376287152081c20f1c3e52abd62f66f93f42ac5a
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-03-21 12:15:56 +0100

    refactor list layout and enhance video display with Bootstrap integration

--- public/js/list.js
+++ public/js/list.js
@@ -1,14 +1,17 @@
+let counter=1
+const container = document.createElement("div")
+container.classList.add("container-fluid")
+container.classList.add("p-5")
 fetch("/test")
-    .then(response => response.json())
-    .then(data => {
-       for (let movie_data of data) {
-            createTitle(movie_data);
-            for (let movie of movie_data.movies) {
-                createVideo(movie);
-            }
-        }
-        addEventToVideos();
-    });
+.then(response => response.json())
+.then(data => {
+    document.body.appendChild(container);
+    
+    for (let movie_data of data) {
+        createRow(movie_data);
+    }
+    addEventToVideos();
+});
 const createVideo = (movie_data) => {
     const type = movie_data.type
     const id = movie_data.ID
@@ -16,12 +19,37 @@ const createVideo = (movie_data) => {
     element.src = `/video/${type}/${id}`;
     element.controls = false;
     element.muted = true;
-    document.body.appendChild(element);
+    return element;
 }
-const createTitle = (movie_data) => {    
+const createTitle = (movie_data) => {
     const element = document.createElement("h1");
     element.textContent = movie_data.type;
     document.body.appendChild(element);
+    return element;
+}
+
+const createRow = (movie_data) => {
+    const title = createTitle(movie_data);
+    const titleCol = document.createElement("div");
+    titleCol.classList.add("col-md-12");
+    const row = document.createElement("div");
+    titleCol.appendChild(title);
+    row.appendChild(titleCol);
+    if(counter%2==0){
+        row.classList.add("colored")
+    }
+
+    const videoCol = document.createElement("div");
+    videoCol.classList.add("col-md-12");
+    row.appendChild(videoCol);
+
+    for (let movie of movie_data.movies) {
+        let video = createVideo(movie);
+        videoCol.appendChild(video);
+    }
+    row.classList.add("row");
+    container.appendChild(row);
+    counter++
 }
 
 const addEventToVideos = () => {
